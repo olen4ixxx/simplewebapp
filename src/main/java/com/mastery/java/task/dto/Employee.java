f327b9c44c1961624ce939b1f882f146981e3ee9
Leanid Nasanovich <olen4ixxx@gmail.com>
2021-12-27 15:12:14 +0300

    v2

--- src/main/java/com/mastery/java/task/dto/Employee.java
+++ src/main/java/com/mastery/java/task/dto/Employee.java
@@ -1,9 +1,18 @@
 package com.mastery.java.task.dto;
 
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.sql.Timestamp;
 import java.util.Objects;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Employee {
 
     private Long employeeId;
@@ -27,90 +36,5 @@ public class Employee {
         this.jobTitle = jobTitle;
         this.gender = gender;
         this.dateOfBirth = dateOfBirth;
-    }
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public Timestamp getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Timestamp dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return Objects.equals(employeeId, employee.employeeId) && Objects.equals(firstName, employee.firstName)
-                && Objects.equals(lastName, employee.lastName) && Objects.equals(departmentId, employee.departmentId)
-                && Objects.equals(jobTitle, employee.jobTitle) && gender == employee.gender
-                && Objects.equals(dateOfBirth, employee.dateOfBirth);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(employeeId, firstName, lastName, departmentId, jobTitle, gender, dateOfBirth);
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "employeeId=" + employeeId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", departmentId=" + departmentId +
-                ", jobTitle='" + jobTitle + '\'' +
-                ", gender=" + gender +
-                ", dateOfBirth=" + dateOfBirth +
-                '}';
     }
 }
